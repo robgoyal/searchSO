323c090310a764dfe55e4cbd11f54ea850d40b34
Robin Goyal <goyal.rob@gmail.com>
2017-08-08 11:44:24 -0400

    Removed test.js and included function to modify DOM with request

--- popup.js
+++ popup.js
@@ -1,43 +1,67 @@
-// Returns the 2 most relevant items from page 1
-var req = new XMLHttpRequest();
-req.open("GET", 'https://api.stackexchange.com/2.2/search/excerpts?page=1&pagesize=2&sort=relevance&q=javascript HTTP get request&site=stackoverflow');
-req.responseType = 'json';
-req.send();
+function sendData(form) {
+    alert("Hello");
+    // Prepare request for stackoverflow
+    var xhr = new XMLHttpRequest();
+    var urlEncodedData = "";
+    var urlEncodedDataPairs = [];
 
-req.onload = function() {
-    var data = req.response;
-
-    var articles = ""
-    for (var step = 0; step < 1; step++) {
-        articles = articles.concat(data['items'][step]['question_id'].toString(), ";");
+    // Add pagesize and search inputs from form to SO API parameters
+    for (var i = 0; i < form.elements.length - 1; i++) {
+        //alert(form.elements[i].name);
+        urlEncodedDataPairs.push(encodeURIComponent(form.elements[i].name) + '=' + encodeURIComponent(form.elements[i].value));
     }
-    articles = articles.concat(data['items'][1]['question_id'].toString(), "?");
-    console.log(articles);
 
-    var requestURL = 'https://api.stackexchange.com/2.2/questions/'.concat(articles, 'site=stackoverflow');
-    console.log(requestURL);
+    // Prepare URL for search query 
+    var baseURL = 'https://api.stackexchange.com/2.2/search/excerpts?page=1&sort=relevance&site=stackoverflow&';
+    urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+    urlEncodedData = baseURL.concat(urlEncodedData);
 
-    var articlereq = new XMLHttpRequest();
-    articlereq.open("GET", requestURL);
-    articlereq.responseType = 'json';
-    articlereq.send();
+    // Send request to receieve article ID's
+    xhr.open('GET', urlEncodedData);
+    xhr.responseType = 'json';
+    xhr.send(null);
 
-    articlereq.onload = function() {
-        var newdata = articlereq.response;
-        console.log(newdata);
-        test(newdata);
+    xhr.onload = function() {
+        var data = xhr.response;
+
+        // Prepare question ID's for next request
+        var questions = "";
+        for (var i = 0; i < data['items'].length - 1; i++) {
+            questions = questions.concat(data['items'][i]['question_id'].toString(), ";");
+        }
+        questions = questions.concat(data['items'][data['items'].length - 1]['question_id'].toString(), "?");
+
+        // 
+        retrievePosts(questions);
     }
+
 }
 
-function test(jsonOBJ){
+function retrievePosts(data) {
+    var requestURL = 'https://api.stackexchange.com/2.2/questions/'.concat(data, 'site=stackoverflow');
 
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', requestURL);
+    xhr.responseType = 'json';
+    xhr.send(null);
 
+    xhr.onload = function() {
+        var posts = xhr.response;
+        modifyHTML(posts);
+    }
+    alert(requestURL);
+}
 
+function modifyHTML(jsonOBJ){
     var myList = document.getElementById('test');
 
-    for (var step = 0; step < 2; step++) {
+    while (myList.firstChild) {
+        myList.removeChild(myList.firstChild);
+    }
+
+    for (var i = 0; i < jsonOBJ['items'].length; i++) {
         var listElem = document.createElement('li');
-        listElem.textContent = jsonOBJ['items'][step]['title'];
+        listElem.textContent = jsonOBJ['items'][i]['title'];
         myList.appendChild(listElem);
     }
 }
